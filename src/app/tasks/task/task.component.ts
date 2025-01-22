9a3c0697910c214969af9f4bf767b3773199a372
TechCommunity-debug <techcommunity611@gmail.com>
2025-01-22 21:51:26 +0530

    Using localStorage for data storage

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -2,7 +2,7 @@ import { Component, inject, Input } from '@angular/core';
 import { type Task } from './task.model';
 import { CardComponent } from "../../shared/card/card.component";
 import { DatePipe } from '@angular/common';
-import { TasksService } from '../task.service';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-task',
