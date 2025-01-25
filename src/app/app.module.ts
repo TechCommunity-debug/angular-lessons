48a3dba1030a303f1f11582dc6ba087b44941915
TechCommunity-debug <techcommunity611@gmail.com>
2025-01-25 13:42:57 +0530

    Using Standalone component in Modules & BrowserModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core'
 import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { HeaderComponent } from './header/header.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [AppComponent],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  imports: [BrowserModule, HeaderComponent, TasksComponent, UserComponent]
 })
 export class AppModule {}
