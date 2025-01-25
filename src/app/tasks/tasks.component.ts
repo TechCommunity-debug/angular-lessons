3af1e1240bab1be6e5ba48112384b19884411e43
TechCommunity-debug <techcommunity611@gmail.com>
2025-01-25 13:29:09 +0530

    Exercise

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+
 import { TaskComponent } from './task/task.component';
 import { NewTaskComponent } from './new-task/new-task.component';
 import { TasksService } from './tasks.service';
@@ -6,9 +7,9 @@ import { TasksService } from './tasks.service';
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.scss',
+  styleUrl: './tasks.component.css',
+  imports: [TaskComponent, NewTaskComponent],
 })
 export class TasksComponent {
   @Input({ required: true }) userId!: string;
@@ -25,7 +26,7 @@ export class TasksComponent {
     this.isAddingTask = true;
   }
 
-  onCloselAddTask() {
+  onCloseAddTask() {
     this.isAddingTask = false;
   }
 }
--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { NewTaskData } from './task/task.model';
+
+import { type NewTaskData } from './task/task.model';
 
 @Injectable({ providedIn: 'root' })
 export class TasksService {
