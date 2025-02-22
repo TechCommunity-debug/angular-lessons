4fbd57e58f8ebc2c8b0b6c527277e1c230891c5d
TechCommunity-debug <techcommunity611@gmail.com>
2025-02-22 17:23:49 +0530

    Managing HTTP-loaded Data via a Service (tap() operator.

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -2,7 +2,7 @@ import { inject, Injectable, signal } from '@angular/core';
 
 import { Place } from './place.model';
 import { HttpClient } from '@angular/common/http';
-import { catchError, map, throwError } from 'rxjs';
+import { catchError, map, throwError, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -24,12 +24,18 @@ export class PlacesService {
     return this.fetchPlaces(
       'http://localhost:3000/user-places',
       'Something went wrong fetching your favourite places. Please try again later.'
+    ).pipe(
+      tap({
+        next: (userPlaces) => this.userPlaces.set(userPlaces),
+      })
     );
   }
 
-  addPlaceToUserPlaces(placeId: string) {
+  addPlaceToUserPlaces(place: Place) {
+    this.userPlaces.update((prevPlaces) => [...prevPlaces, place]);
+
     return this.httpClient.put('http://localhost:3000/user-places', {
-      placeId,
+      placeId: place.id,
     });
   }
 
