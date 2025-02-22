2dec60aae3a85c44500dbe42fb8c091e8b875908
TechCommunity-debug <techcommunity611@gmail.com>
2025-02-22 12:57:13 +0530

    Change Project: Http requests & responses.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,75 +1,13 @@
-import {
-  Component,
-  computed,
-  DestroyRef,
-  effect,
-  inject,
-  OnInit,
-  signal,
-} from '@angular/core';
-import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { Component } from '@angular/core';
 
-import { interval, map, Observable } from 'rxjs';
+import { AvailablePlacesComponent } from './places/available-places/available-places.component';
+import { UserPlacesComponent } from './places/user-places/user-places.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  imports: [AvailablePlacesComponent, UserPlacesComponent],
 })
-export class AppComponent implements OnInit {
-  clickCount = signal(0);
-  clickCount$ = toObservable(this.clickCount);
-  interval$ = interval(1000);
-  intervalSignal = toSignal(this.interval$, { initialValue: 0 });
-  customInterval$ = new Observable((subscriber) => {
-    // subscriber.error();
-    let timesExecuted = 0;
-    const interval = setInterval(() => {
-      if (timesExecuted > 5) {
-        clearInterval(interval);
-        subscriber.complete();
-        return;
-      }
-      console.log('Emitting new value...');
-      subscriber.next({ message: 'new value' });
-      timesExecuted++;
-    }, 2000);
-  });
-  private destroyRef = inject(DestroyRef);
-
-  constructor() {
-    // toObservable(this.clickCount);
-  }
-
-  ngOnInit(): void {
-    // setInterval(() => {
-    //   this.interval.update((prevIntervalNumber) => prevIntervalNumber + 1);
-    // }, 1000);
-
-    // const subscription = interval(1000)
-    //   .pipe(map((val) => val * 2))
-    //   .subscribe({
-    //     next: (val) => console.log(val),
-    //   });
-
-    // this.destroyRef.onDestroy(() => {
-    //   subscription.unsubscribe();
-    // });
-    this.customInterval$.subscribe({
-      next: (val) => console.log(val),
-      complete: () => console.log('COMPLETED!'),
-      error: () => console.log('ERROR!'),
-    });
-    const subscription = this.clickCount$.subscribe({
-      next: (val) => console.log(`Clicked button ${this.clickCount()} times.`),
-    });
-
-    this.destroyRef.onDestroy(() => {
-      subscription.unsubscribe();
-    });
-  }
-
-  onClick() {
-    this.clickCount.update((prevCount) => prevCount + 1);
-  }
-}
+export class AppComponent {}
