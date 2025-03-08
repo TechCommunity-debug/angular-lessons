2cb7deb7ee5bfd7431597146cc2c2b7d2a7fa251
TechCommunity-debug <techcommunity611@gmail.com>
2025-03-08 16:24:29 +0530

    Routing: way 2: Accessing Route Data In Components.

--- src/app/users/user-tasks/user-tasks.component.ts
+++ src/app/users/user-tasks/user-tasks.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, input } from '@angular/core';
 import { UsersService } from '../users.service';
 import {
+  ActivatedRoute,
   ActivatedRouteSnapshot,
   ResolveFn,
   RouterLink,
@@ -18,6 +19,15 @@ import {
 export class UserTasksComponent {
   userName = input.required<string>();
   message = input.required<string>();
+//   private activatedRoute = inject(ActivatedRoute);
+
+//   ngOnInit(): void {
+//     this.activatedRoute.data.subscribe({
+//       next: data => {
+//         console.log(data);
+//       }
+//     })
+//   }
 }
 
 export const resolveUserName: ResolveFn<string> = (
